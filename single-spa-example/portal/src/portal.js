ce6599449c6c7adb2154737fc24f0faf926d0b6b
Elias Dräxler <elias.draexler@twinformatics.at>
2018-10-31 17:37:47 +0100

    added notes & fixed vue loading

--- single-spa-example/portal/src/portal.js
+++ single-spa-example/portal/src/portal.js
@@ -8,7 +8,7 @@ async function init() {
     const loadingPromises = [];
 
     // The URLs /app1 - /app4 are redirected to different ports by the webpack proxy (webpack.config.js)
-    loadingPromises.push(singleSpa.registerApplication('vueNavBar', () => SystemJS.import('/app4/singleSpaEntry.js'), () => true, null));
+    loadingPromises.push(loadApp('vueNavBar', null, '/app4/singleSpaEntry.js', null, null));
     loadingPromises.push(loadApp('app1', null, '/app1/singleSpaEntry.js', '/app1/store.js', globalEventDistributor));
     loadingPromises.push(loadApp('app2', null, '/app2/singleSpaEntry.js', '/app2/store.js', globalEventDistributor));
     loadingPromises.push(loadApp('app3', null, '/app3/singleSpaEntry.js', null, null)); // does not have a store, so we pass null
